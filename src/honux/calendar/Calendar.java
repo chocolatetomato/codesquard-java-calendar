4bb9174d053159a6dd523b6d8582d8e116677be1
jade <jade@DESKTOP-CCA17RT>
2021-11-28 20:16:17 +0900

    Update using class method

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -3,8 +3,12 @@ package honux.calendar;
 import java.util.Scanner;
 
 public class Calendar {
-
-	public static void main(String[] args) {
+	
+	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	public int getMaxDaysOfMonth(int month) {
+		return MAX_DAYS[month - 1];
+	}
+	public void printSampleCalendar() {
 		System.out.println("일  월  화  수  목  금  토");
 		System.out.println("--------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
@@ -12,6 +16,10 @@ public class Calendar {
 		System.out.println("15 16 17 18 19 20 21"); 
 		System.out.println("22 23 24 25 26 27 28");
 		System.out.println();
+		
+	}
+	
+	public static void main(String[] args) {
 		
 		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
 
@@ -40,13 +48,16 @@ public class Calendar {
 //		System.out.printf("%d월의 최대 일수는 %d일 입니다",month, max);
 //		sc.close();
 		
+		Calendar cal = new Calendar();
 		int month = sc.nextInt();
 		
-		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 		
-		System.out.printf("%d월은 %d일까지 있습니다.", month, maxDays[month - 1]);
+		System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+		
+		cal.printSampleCalendar();
+		
 		sc.close();
 		
 	}
-
 }
+
